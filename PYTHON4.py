cdcb62c170fbfb46358ff1f5e6ca3f7cd1f783f0
Mansoorbasha331 <162982681+Mansoorbasha331@users.noreply.github.com>
2024-03-14 16:01:00 +0530

    Add files via upload

---
+++ PYTHON4.py
@@ -0,0 +1,140 @@
+
+# ----->whil loop
+# ------> break using while loop
+
+# eg:1
+# 1.) Iterate from 20 to 30 and break the loop in 27
+
+#i = 20
+while i< 31:
+    if i ==27:
+        break
+    print(i)
+   i+=
+#i = 20
+#while i<31:
+#    print(i
+#     i+=1
+#    if i==27:
+#      break
+
+
+# 3.)
+#i = 20
+#while i<31:
+#    print(i)
+#    if i==27:
+#      break
+#      i+=1
+
+
+# i = 20
+# while i<31:
+#     print(i)
+#     if i==27:
+#       break
+#       i+=1
+
+# # if i==27:
+# continue
+# print(i)
+# ! eg:5
+# while to iterate from 12 to 22
+# fimd the sum of all number
+
+i =12
+ while i<23:
+
+     sum=sum+i
+     print(sum)
+     i+=1
+     # print (sum)
+     #!Eg:6
+     # Find the average of value from 20 to 30
+     i=20
+     sum=0
+     count=0
+     while i<30
+     sum = 0
+     # count+=1
+     # i+1
+     # print(sum/count)
+     #!---->Nested for loop
+     # Eg:1
+     # for rpw in range (1,3+1):
+     # for col in range (1,4+1):
+     #  print(row,co1)
+     #Eg:2
+     #* * * * *
+     #* * * * *
+     #* * * * *
+     #* * * * *
+     [10:42 am, 14/03/2024] Shivaji Ksrm: # i = 20
+# while i<31:
+#     i=i+1
+#     if i==27:
+#         continue
+#     print(i)
+
+# ! Eg:5
+# while o iterate from 12 to 22
+# find the sum of all numbers
+[11:59 am, 14/03/2024] Shivaji Ksrm: # Eg:2
+# * * * *
+# * * * *
+# * * * *
+# * * * *
+
+row = int(inputt("Enter the rows: "))
+cols = int(input("enter the cols: "))
+
+for ow in range(rows):
+    forb col in range(cols):
+        print("*", end=" ")
+    print()
+    #* * * * * * *
+    #* * * * * * 
+    #* * * * *
+    #* * * *
+    #* *
+    #*
+    # for col in range (0,row ):
+    # print ("*",end=" ")
+    #else 
+    
+
+#    * * *
+#   * * * *
+#  * * * * *
+ #for row in range (0,5);
+#    for col in range (0,6):
+#      if ((row==0and col >==3 )or (row==1and (c0l<=4)or
+#(row==2and (c
+    
+     # * charactristics of list
+# 1.) list have to be sorrounded by []
+# 2.) it is mutable (the elements in the list are changable)
+# 3.) Every element inside list is indexed
+# 4.) The elements inside list will be ordered format
+# 5.) it can hold duplicate values
+# 6.) its heterogeneous
+
+# to access the elements in list
+11 = [1, 4, 1, 7, 89, 7, 7, 5, "p", "i"]
+# print(11)
+
+# ---> Indexing
+#in the collection data types like list ,tuple ,string the elements will be alloted
+#with pri defined unique value
+#we have 2 types of indexing
+#positive indexing ------> starts with 0 from left and side
+print
+#print(1st1[-7:-1:2])
+#! To insert ot add element inside list
+# append()-> used to add elelement at last position of list
+# 11 [9, 8, 0, 6]
+# 11.append("car")
+#print(11)
+
+
+
